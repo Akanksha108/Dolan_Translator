06cc3b02194be4be0e5b7b3557a16baad0338f32
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-07-07 17:38:10 +0530

    Testing addEventListener function

---
+++ script.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.getElementById("btn-translate");
+
+var textInput = document.getElementsByClassName("text-input");
+
+var textOutput = document.querySelector(".text-output");
+
+btnTranslate.addEventListener("click", function(){
+    console.log("Testing");
+});
