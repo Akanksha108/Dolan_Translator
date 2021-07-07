611844def7c2230c456f6201acf01390dd538565
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-07-07 17:31:57 +0530

    Revert "Accessed DOM Elements"
    This reverts commit 8b8d96f59c730a7bd110d231a3723ec11c17e915.

--- script.js
+++
@@ -1,6 +0,0 @@
-var btnTranslate =  document.getElementById("btn-translate");
-
-var textInput = document.getElementsByClassName("text-input");
-
-var textOutput = document.querySelector(".text-output");
-
