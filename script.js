8b8d96f59c730a7bd110d231a3723ec11c17e915
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-07-07 17:30:30 +0530

    Accessed DOM Elements

---
+++ script.js
@@ -0,0 +1,6 @@
+var btnTranslate =  document.getElementById("btn-translate");
+
+var textInput = document.getElementsByClassName("text-input");
+
+var textOutput = document.querySelector(".text-output");
+
