89acb86e865c0b17ca6c410a68c9c283bf7cbba6
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-07-07 19:46:08 +0530

    added function to translate

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 var btnTranslate = document.getElementById("btn-translate");
 
-var textInput = document.getElementsByClassName("text-input");
+var textInput = document.querySelector(".text-input");
 
 var textOutput = document.querySelector(".text-output");
 
-btnTranslate.addEventListener("click", function(){
-    console.log("Testing");
-});
+var serverURL = "https://api.funtranslations.com/translate/dolan.json";
+
+
+function getTranslationalURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log(`Error Occured ${error}`);
+}
+
+function clickHandler(){
+
+    console.log(textInput.value);
+
+    fetch(getTranslationalURL(textInput.value))
+    .then(response => response.json())
+    .then(json => {
+       var translatedText = json.contents.translated;
+       textOutput.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler);
